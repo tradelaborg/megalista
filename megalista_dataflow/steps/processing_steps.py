cb52682885f25646b638c7778b6d669c0529381a
Dia Al Jrab <dia.aljrab@jellyfish.com>
2024-11-28 18:24:01 +0100

    Update processing_steps.py

--- megalista_dataflow/steps/processing_steps.py
+++ megalista_dataflow/steps/processing_steps.py
@@ -182,7 +182,7 @@ class GoogleAdsCustomerMatchMobileDeviceIdStep(MegalistaStep):
                 ),
                 self.params.dataflow_options,
                 DestinationType.ADS_CUSTOMER_MATCH_MOBILE_DEVICE_ID_UPLOAD,
-                100000,
+                50000,
             )
             | "Hash Users - Google Ads Customer Match Contact Info"
             >> beam.Map(ADS_CM_HASHER.hash_users)
@@ -212,7 +212,7 @@ class GoogleAdsCustomerMatchContactInfoStep(MegalistaStep):
                 ),
                 self.params.dataflow_options,
                 DestinationType.ADS_CUSTOMER_MATCH_CONTACT_INFO_UPLOAD,
-                100000,
+                50000,
             )
             | "Hash Users - Google Ads Customer Match Contact Info"
             >> beam.Map(ADS_CM_HASHER.hash_users)
@@ -242,7 +242,7 @@ class GoogleAdsCustomerMatchUserIdStep(MegalistaStep):
                 ),
                 self.params.dataflow_options,
                 DestinationType.ADS_CUSTOMER_MATCH_USER_ID_UPLOAD,
-                100000,
+                50000,
             )
             | "Hash Users - Google Ads Customer Match Contact Info"
             >> beam.Map(ADS_CM_HASHER.hash_users)
